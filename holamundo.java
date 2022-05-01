40bfa3b95e588fc064022a44da70e1ba97b0da66
Fran Guido Felix Gomez <ffelixg@ulasalle.edu.pe>
2022-04-30 22:14:11 -0500

    Hola Mundo mejorado

--- holamundo.java
+++ holamundo.java
@@ -3,7 +3,7 @@
       public static void main(String args[]) {
       
           /** */
-          System.out.println ("¡Hola mundo!");
+          System.out.println ("¡Hola mundo numero 2!");
 
       }
   }
